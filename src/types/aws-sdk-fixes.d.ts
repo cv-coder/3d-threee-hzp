640420a6b6c3824b58f3e35e5481724a7e54253d
普通市民梁生 <1874764803@qq.com>
2026-03-28 15:17:23 +0800

    feat: 添加 Radix UI 的 Label 和 Slider 组件类型定义

--- src/types/aws-sdk-fixes.d.ts
+++ src/types/aws-sdk-fixes.d.ts
@@ -19,3 +19,47 @@ declare module '@radix-ui/react-slot' {
     SlotProps & React.RefAttributes<HTMLElement>
   >;
 }
+
+declare module '@radix-ui/react-label' {
+  import * as React from 'react';
+
+  export interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
+    asChild?: boolean;
+  }
+
+  export const Root: React.ForwardRefExoticComponent<
+    LabelProps & React.RefAttributes<HTMLLabelElement>
+  >;
+}
+
+declare module '@radix-ui/react-slider' {
+  import * as React from 'react';
+
+  export interface SliderProps extends React.HTMLAttributes<HTMLSpanElement> {
+    value?: number[];
+    defaultValue?: number[];
+    min?: number;
+    max?: number;
+    step?: number;
+    disabled?: boolean;
+    orientation?: 'horizontal' | 'vertical';
+    dir?: 'ltr' | 'rtl';
+    inverted?: boolean;
+    minStepsBetweenThumbs?: number;
+    onValueChange?: (value: number[]) => void;
+    onValueCommit?: (value: number[]) => void;
+  }
+
+  export const Root: React.ForwardRefExoticComponent<
+    SliderProps & React.RefAttributes<HTMLSpanElement>
+  >;
+  export const Track: React.ForwardRefExoticComponent<
+    React.HTMLAttributes<HTMLSpanElement> & React.RefAttributes<HTMLSpanElement>
+  >;
+  export const Range: React.ForwardRefExoticComponent<
+    React.HTMLAttributes<HTMLSpanElement> & React.RefAttributes<HTMLSpanElement>
+  >;
+  export const Thumb: React.ForwardRefExoticComponent<
+    React.HTMLAttributes<HTMLSpanElement> & React.RefAttributes<HTMLSpanElement>
+  >;
+}
